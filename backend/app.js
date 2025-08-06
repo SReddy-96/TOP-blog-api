93e8b4b6c37eaf4b2d9371ef479d5042c758c1b2
Steven Reddy <steveng.reddy@gmail.com>
2025-08-06 16:18:25 +1200

    fix: CORS issue with multiple frontend urls

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,11 @@ const app = express();
 // enable CORS
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || process.env.FRONTEND_ADMIN_URL ||"http://localhost:5173", // your frontend URL
+    origin: [
+      process.env.FRONTEND_URL,
+      process.env.FRONTEND_ADMIN_URL,
+      "http://localhost:5173",
+    ].filter(Boolean),
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
