ff2deac9edc7b09ce8b8888e9da397deabe533f6
Steven Reddy <steveng.reddy@gmail.com>
2025-08-06 16:05:17 +1200

    fix: cors issue with admin url

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ const app = express();
 // enable CORS
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173", // your frontend URL
+    origin: process.env.FRONTEND_URL || process.env.FRONTED_ADMIN_URL ||"http://localhost:5173", // your frontend URL
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
